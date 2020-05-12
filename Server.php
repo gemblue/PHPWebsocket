12ffe753e956c401222a649bd76a6a7858089978
oriza <ahmadoriza@gmail.com>
2020-05-12 14:27:55 +0700

    Init simple websocket server

---
+++ Server.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
+
+/**
+ * WebSocket 
+ * 
+ * PHP WebSocket simple server, just wrap a PHP Socket API.
+ * For learning purpose. This class was modified from reference tutorial, making procedural code to OOP.
+ * 
+ * Reference :
+ * https://medium.com/@cn007b/super-simple-php-websocket-example-ea2cd5893575
+ * 
+ * @package WebSocket
+ * @author Gemblue
+ *
+ */
+
+class WebSocket {
+
+}
